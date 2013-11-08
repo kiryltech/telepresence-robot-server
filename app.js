68d6a84d6c066bfd1e5f112dfe30508965e69437
Kirill Duborenko <k.duborenko@gmail.com>
2013-11-07 17:41:24 -0800

    Import project

---
+++ app.js
@@ -0,0 +1,88 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.set('port', port);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server, { log: false });
+
+server.listen(app.get('port'), ipaddress, function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var clients = [];
+var robot = null;
+
+io.sockets.on('connection', function (socket) {
+  clients = clients.concat(socket);
+  socket.emit('robotStatus', robot ? "connected" : "disconnected");
+  socket.on('action', function (data) {
+    if (robot) {
+      robot.write(data.requestId + ":" + data.action.toUpperCase() + "\r\n");
+    }
+  });
+});
+
+io.sockets.on('disconnect', function (socket) {
+  var index = clients.indexOf(socket);
+  if (index > -1) {
+    clients.splice(index);
+  }
+});
+
+var net = require('net');
+var robotServer = net.createServer(function (socket) {
+  socket.setEncoding("utf-8");
+  socket.on('data', function (data) {
+    data = data.split("\n");
+    data.forEach(function (requestId) {
+      if (requestId) {
+        clients.forEach(function (socket) {
+          socket.emit('actionResult', {
+            requestId: requestId,
+            status: "OK"
+          });
+        });
+      }
+    })
+  });
+  socket.on('end', function () {
+    robot = null;
+    clients.forEach(function (socket) {
+      socket.emit('robotStatus', "disconnected");
+    });
+  });
+  robot = socket;
+  clients.forEach(function (socket) {
+    socket.emit('robotStatus', "connected");
+  });
+});
+
+var robotServerPort = 9999;
+robotServer.listen(robotServerPort, function () {
+  console.log('Robot server listening on port ' + robotServerPort);
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,49 @@
+(function () {
+  var requestId = 0;
+  var requestsTimestamps = {};
+  var socket = null;
+
+  this.main = function () {
+    connectToServer();
+    var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      button.addEventListener('click', action)
+    }
+  };
+
+  function connectToServer() {
+    socket = io.connect('http://localhost');
+    socket.on('robotStatus', function (data) {
+      document.getElementById('robotStatus').innerHTML = data;
+    });
+    socket.on('actionResult', function (data) {
+      console.log("Time: " + (new Date().getTime() - requestsTimestamps[data.requestId]) + "ms, response:", data);
+      delete requestsTimestamps[data.requestId]
+    });
+  }
+
+  function action(event) {
+    var element = event.target;
+    var rId = requestId++;
+    requestsTimestamps[rId] = new Date().getTime();
+    socket.emit('action', {action: element.id, requestId: rId})
+  }
+
+  var actions = {
+    w: "forward",
+    s: "backward",
+    a: "left",
+    d: "right"
+  };
+
+  window.addEventListener('keypress', function (e) {
+    var action = actions[String.fromCharCode(e.charCode)];
+    if (action) {
+      var el = document.getElementById(action);
+      var evObj = document.createEvent('Events');
+      evObj.initEvent('click', true, false);
+      el.dispatchEvent(evObj);
+    }
+  });
+})();
